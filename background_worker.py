ef5922a452b787f1fe083205c66eadc8f455ca54
matolgithub <89175955650@mail.ru>
2022-11-26 19:15:45 +0300

    Finish commit.

--- background_worker.py
+++ background_worker.py
@@ -1,7 +1,12 @@
 import redis
 
 redis_client = redis.Redis(host="127.0.0.1", port=6379)
-while True:
-    with redis_client as client:
-        math_express_waiting = client.brpop("math_expression")
-        print(math_express_waiting, type(math_express_waiting))
+with redis_client as client:
+    while True:
+        math_express_waiting = client.brpop("math_expression")[1].decode(encoding="utf-8")
+        # print(math_express_waiting, type(math_express_waiting))
+
+        answer = round(float(eval(math_express_waiting)), 2)
+        print(answer, type(answer))
+
+        client.lpush("answers", answer)
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ redis_client = redis.Redis(host="127.0.0.1", port=6379)
 while True:
     with redis_client as client:
         math_expression = input("Enter math expression: ")  # 2+5-1
-        if math_expression == "q":
-            break
-        else:
+        if math_expression != "q":
             client.lpush("math_expression", math_expression)
+        else:
+            break
+
+        calculate_value = client.brpop("answers")[1].decode(encoding="utf-8")
+        print(calculate_value)
